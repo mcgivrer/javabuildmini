6b546d9d2ceacd78531b4f55b84c67d530cc55d8
Frederic Delorme <frederic.delorme@gmail.com>
2025-04-27 17:22:05 +0200

    add IDEA support

--- src/main/java/tutorials/App.java
+++ src/main/java/tutorials/App.java
@@ -1,12 +1,29 @@
 package tutorials;
 
+import java.time.ZonedDateTime;
+import java.util.ResourceBundle;
+
 public class App {
+  public static ResourceBundle messages = ResourceBundle.getBundle("i18n/messages.properties");
+  
   public void App(){
+    info(App.class, "Stat the application %s",messages.getString("app.title"));
   }
+  
   public void run(String[] args){
   }
+  
   public static void main(String[] args){
     App app = new App();
     app.run(args);
   }
+
+  public static void log(Class cls, String level, String message, Object... args){
+    System.out.printf("%s;%s;%s;%s%f", ZonedDateTime.now(), cls.getCanonicalName(),level, message.formatted(args));
+  }
+
+  public static void info(Class cls, String message, Object... args){
+    log(cls,"INFO",message,args);
+  }
+    
 }
