ade6b3cd098f29658c8065e033d49912ab9939bb
Frederic Delorme <frederic.delorme@gmail.com>
2025-05-16 13:39:20 +0200

    feat(vfx): Update Day-Night with realistic Sun moves
    - Sun moves in the sky accordingly to seasons.

--- src/main/java/tutorials/Sun.java
+++ src/main/java/tutorials/Sun.java
@@ -4,13 +4,16 @@ import tutorials.Entity;
 import tutorials.World;
 
 import java.awt.*;
+import java.time.LocalDate;
 
 public class Sun extends Entity {
 
     private World world;
+    private double latitude = 45.0; // Latitude de Paris
 
     public Sun(String name) {
         super(name);
+        setSize(80, 80);
     }
 
     public Sun setWorld(World w) {
@@ -25,7 +28,7 @@ public class Sun extends Entity {
         else return (18f - heure) / 6f;                  // 12h -> 18h : 1 -> 0
     }
 
-    private Point getSunPosition(int width, int height, float heure) {
+    private Point getOldSunPosition(int width, int height, float heure) {
         // Paramètres de la "journée"
         float heureLever = 6f;
         float heureCoucher = 21f;
@@ -54,11 +57,42 @@ public class Sun extends Entity {
         return new Point(x, y);
     }
 
+    private Point getSunPosition(int width, int height, float heure) {
+        LocalDate date = LocalDate.now();
+        int jourAnnee = date.getDayOfYear();
+
+        // 1. Calcul déclinaison solaire (formule simplifiée)
+        double delta = Math.toRadians(23.45 * Math.sin(Math.toRadians(360 * (284 + jourAnnee) / 365.0)));
+
+        // 2. Calcul durée du jour
+        double phi = Math.toRadians(latitude);
+        double heuresLumiere = 2 * Math.acos(-Math.tan(phi) * Math.tan(delta)) * 12 / Math.PI;
+
+        // 3. Heures lever/coucher dynamiques
+        float lever = (float) (12 - heuresLumiere / 2);
+        float coucher = (float) (12 + heuresLumiere / 2);
+
+        // 4. Position horizontale et verticale
+        if (heure < lever || heure > coucher)
+            return new Point((int)world.getWidth() / 2, (int)world.getHeight()+100); // Soleil caché
+
+        float progression = (heure - lever) / (coucher - lever);
+        int x = (int)world.getWidth() / 2 + (int) (width * 0.3 * Math.sin(progression * Math.PI - Math.PI / 2));
+        int y = (int) ((int)world.getHeight() * 1.0 - world.getHeight() * 0.8 * Math.cos(delta) * Math.sin(progression * Math.PI));
+
+        return new Point(x, y);
+    }
+
+    @Override
+    public void update(long elapsed) {
+        float heure = world.getDayTime();
+        Point sunPos = getSunPosition((int) world.getWidth(), (int) world.getHeight(), heure);
+        this.x = sunPos.x - width * 0.5;
+        this.y = sunPos.y - height * 0.5;
+    }
 
     public void draw(Graphics2D g2) {
         float heure = world.getDayTime();
-        Point sunPos = getSunPosition((int) world.getWidth(), (int) world.getHeight(), heure);
-        int radius = 50;
         float brightness = getSunBrightness(heure);
         Color colorHorizon = new Color(255, 200, 80, 220); // Jaune/orangé
         Color colorZenith = new Color(255, 255, 255, 240);
@@ -66,7 +100,7 @@ public class Sun extends Entity {
         Color sunColor = Utils.interpolateColor(colorHorizon, colorZenith, brightness);
 
         g2.setColor(sunColor);
-        g2.fillOval(sunPos.x - radius, sunPos.y - radius, 2 * radius, 2 * radius);
+        g2.fillOval((int) x, (int) y, (int) width, (int) height);
     }
 
 }
